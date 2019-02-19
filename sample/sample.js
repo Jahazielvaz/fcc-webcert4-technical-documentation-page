48bb52970f1da8fb5f074ba830be6d76eef4a2fe
Jahaziel <jahaziel.vaz@gmail.com>
2019-02-18 22:59:04 -0700

    playground finished

--- sample/sample.js
+++ sample/sample.js
@@ -1,9 +1,14 @@
 function validateForm(){
   var formValues = document.forms[0]["your-name"].value;
 
-  if(formValues == ""){
+  if(formValues === ""){
     alert("Please include your name");
     document.getElementById('name').style.border = '1px solid red';
     return false;
+  } else if (formValues >= -Infinity){
+    alert("Please makes sure your name doesn't include numbers");
+    return false;
+  } else {
+    return document.write("Welcome To The Team");
   }
 }
